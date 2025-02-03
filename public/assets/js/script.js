080731d71f3448240fb6958e3a99b93ef2e374c6
Yuka413 <siba0214koro@gmail.com>
2025-02-04 00:49:35 +0900

    header完成

---
+++ public/assets/js/script.js
@@ -0,0 +1,5 @@
+$("#js-header-button").on("click", function () {
+  $(".l-header__menu").toggleClass("is-active");
+  $(".l-header__nav").toggleClass("is-active");
+  $(".l-header").toggleClass("is-active");
+});
