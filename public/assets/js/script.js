22e9af8d519adc290690f1bebc238f56922bb3dd
Yuka413 <siba0214koro@gmail.com>
2025-02-09 23:08:08 +0900

    パーフェクトピクセル確認

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,39 @@
-$("#js-header-button").on("click", function () {
-  $(".l-header__menu").toggleClass("is-active");
-  $(".l-header__nav").toggleClass("is-active");
-  $(".l-header").toggleClass("is-active");
+// メニュークリックで開閉
+$(document).ready(function () {
+  const headerNav = $(".l-header__nav");
+  const navHeight = headerNav.outerHeight();
+
+  $("#js-header-button").on("click", function () {
+    $(this).toggleClass("is-active");
+    if ($("#js-header-button").hasClass("is-active")) {
+      $("#js-header-button").css("transform", "translateY(0)");
+      $(".l-header__container").removeClass("is-active");
+    } else {
+      $("#js-header-button").css({
+        transform: `translateY(${navHeight}px)`,
+        transition: "transform 0.3s ease",
+      });
+      $(".l-header__container").addClass("is-active");
+    }
+  });
+});
+
+
+
+
+// スムーススクロール
+$('a[href^="#"]').on("click", function (e) {
+  const speed = 1000;
+  const id = jQuery(this).attr("href");
+  const paddingTop = 30;
+  const target = jQuery("#" == id ? "html" : id);
+  const position = jQuery(target).offset().top - paddingTop;
+  $("html, body").animate({
+    scrollTop: position,
+  },
+  speed,
+  "swing"
+);
+$("#js-header-button").css("transform", "translateY(0)");
+$(".l-header__container").removeClass("is-active");
 });
